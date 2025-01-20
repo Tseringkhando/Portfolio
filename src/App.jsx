5dba973ecedc3fde6018d467c4c2a53526e92c78
Tseringkhando <candy.khando@gmail.com>
2025-01-19 23:25:01 -0500

    change in bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Background from "./Background/Background";
 import Footer from "./sections/Footer/Footer";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
@@ -6,6 +7,7 @@ import Skills from "./sections/Skills/Skills";
 function App() {
   return (
     <>
+      <Background />
       <Hero />
       <Projects />
       <Skills />
